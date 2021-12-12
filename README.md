89d652b2a7f0d4ecb08e40424cb402a847f7a7ff
Unknown <tomatoesneverlie@gmail.com>
2021-12-12 00:42:47 -0500

    wrote linker

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# linker-dot-md
+# linker.md
+
+an incredibly basic linker made for markdown files
+
+
+#### Usage:
+
+Either
+
+**1.** link with `./linker.py [SOURCE] [DEST]` or `./linker.py [SOURCE]` (which will print results to stdout), or
+
+**2.** use it through python (or whatever you want to FFI that to)
+> ```python
+> from linker import link
+> open('dest.md', 'w').writelines(link(open('src.md', 'r'))) 
+> ```
+
+Files referenced with `!require(relative/path/to/file)` will be inserted. `!req()` and `!r()` also should do the trick, or you can specify your own syntax with a regex pattern.
---
+++ linker.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import re
+import sys
+import os
+# <REGEX DEFINITIONS>
+# identifying the start and end of code-blocks
+block_start   = re.compile("^```(python|c|rust|bash|cpp|c\+\+|go|js|javascript|lua)#run( *#\w*( *= *[\w.]*)?)*$")
+block_end     = re.compile("^```$")
+
+block_unboxed = re.compile(".*#unboxed")
+block_new     = re.compile(".*#new")
+block_hide    = re.compile(".*#hide")
+# </REGEX DEFINITIONS>
+
+# <MAIN PROCESS>
+def link(file, depth=3, pattern="^!r(?:eq(?:uire)?)?\(([\w\-. \/]+)\)$"):
+    """ link a markdown file
+
+    Input: the path to a file
+    Output: A list of strings, the resulting file.
+
+    The default pattern matches `!require(...)` or `!r(...)`, with a path
+    relative to the source file.
+    """
+    pattern = re.compile(pattern)
+    lines = []
+
+    if len(fdir := os.path.dirname(file)) > 0:
+        os.chdir(fdir)
+    with open(os.path.basename(file), "r") as f:
+        source_lines = f.readlines()
+        for i, line in enumerate(source_lines):
+            if depth > 0 and (match:=pattern.match(line)):
+                lines += link(match.group(1), depth-1)
+            else: lines.append(line)
+    return lines
+# </MAIN PROCESS>
+
+
+# <CLI INVOCATION>
+if __name__ == '__main__':
+    result = link(sys.argv[1])
+    # Output the results
+    if len(sys.argv) == 3:
+        with open(sys.argv[2], 'w') as dest:
+            for line in result:
+                dest.write(line)
+    else:
+        for line in result:
+            print(line, end='')
+# </CLI INVOCATION>
